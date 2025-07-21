170b000ee17c0782f72541479db93a29b1daec00
chisonm <danielchisonum@gmail.com>
2025-07-22 02:20:12 +0300

    project initialized

---
+++ resources/js/components/app-sidebar.tsx
@@ -0,0 +1,81 @@
+import { NavFooter } from '@/components/nav-footer';
+import { NavMain } from '@/components/nav-main';
+import { NavUser } from '@/components/nav-user';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/react';
+import { BookOpen, ChartAreaIcon, Folder, LayoutGrid, ListPlusIcon, MonitorCog, Orbit, User2Icon } from 'lucide-react';
+import AppLogo from './app-logo';
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+        icon: LayoutGrid,
+    },
+      {
+        title: 'Analytics',
+        href: '/analytics',
+        icon: ChartAreaIcon,
+    },
+      {
+        title: 'User Management',
+        href: '/user-management',
+        icon: User2Icon,
+    },
+      {
+        title: 'Products Management',
+        href: '/product-management',
+        icon: Folder,
+    },
+      {
+        title: 'Payments',
+        href: '/payments',
+        icon: MonitorCog,
+    },
+        {
+        title: 'Orders',
+        href: '/orders',
+        icon: ListPlusIcon,
+    },
+];
+
+const footerNavItems: NavItem[] = [
+    {
+        title: 'Repository',
+        href: 'https://github.com/laravel/react-starter-kit',
+        icon: Folder,
+    },
+    {
+        title: 'Documentation',
+        href: 'https://laravel.com/docs/starter-kits#react',
+        icon: BookOpen,
+    },
+];
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon" variant="sidebar">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href="/dashboard" prefetch>
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarContent>
+                <NavMain items={mainNavItems} />
+            </SidebarContent>
+
+            <SidebarFooter>
+                {/* <NavFooter items={footerNavItems} className="mt-auto" /> */}
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
+}
